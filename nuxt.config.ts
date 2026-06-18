82dd745e35ffcd8490c7dbf3f8b3660b17caaa70
dragon-fish <dragon-fish@qq.com>
2026-06-18 17:21:08 +0800

    fix: add optimizeDeps configuration for improved dependency management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,16 @@ export default defineNuxtConfig({
     esbuild: {
       drop: process.env.NODE_ENV === 'production' ? ['console'] : [],
     },
+    optimizeDeps: {
+      include: [
+        '@vicons/fa',
+        '@vicons/utils',
+        'axios',
+        'localforage', // CJS
+        'naive-ui',
+        'nprogress', // CJS
+      ],
+    },
   },
 
   compatibilityDate: '2026-06-18',
